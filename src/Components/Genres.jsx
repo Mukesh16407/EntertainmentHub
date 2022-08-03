0a0eb33152e7bb37fd9560dac2b010844168658e
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-08-03 20:50:50 +0530

    Movies page

---
+++ src/Components/Genres.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import Chip from '@mui/material/Chip';
+import axios from 'axios';
+
+export const Genres = ({
+  selectedGenres,
+  setSelectedGenres,
+  genresData,
+  setGenresData,
+  type,
+  setPage,
+}) => {
+
+const handleAdd =(genre)=>{
+  setSelectedGenres([...selectedGenres,genre]);
+  setGenresData(genresData.filter((g)=> g.id !==genre.id));
+  setPage(1)
+}
+
+const handleRemove = (genre) => {
+  setSelectedGenres(
+    selectedGenres.filter((selected) => selected.id !== genre.id)
+  );
+  setGenresData([...genresData, genre]);
+  setPage(1);
+};
+
+const fetchGenresData= async()=>{
+
+  try{
+    const {data} =  await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    setGenresData(data.genres)
+
+  }catch(err){
+    console.log(err)
+  }
+}
+useEffect(()=>{
+  fetchGenresData() 
+  
+  return () => {
+    setGenresData({}); // unmounting
+  };
+  // eslint-disable-next-line
+},[])
+  
+  return (
+  <div style={{padding:"6px 0px" }}>
+   {selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {genresData.map((genre) => (
+        <Chip
+          style={{ margin: 2,backgroundColor:"white" }}
+          label={genre.name}
+          key={genre.id}
+          clickable
+          size="small"
+          onClick={() => handleAdd(genre)}
+        />
+      ))}
+  </div>
+  );
+};
--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { SingleContent } from '../Components/SingleContent/SingleContent';
+import { CustomPagination } from '../Components/Pagination/CustomPagination';
+import { Genres } from '../Components/Genres';
+import useGenres from '../Hooks/useGenre';
 
 export const Movies = () => {
+  
+  const [content, setContent] = useState([]);
+  const [page, setPage] = useState(1);
+  const [numOfPages, setNumOfPages] = useState();
+  const [genresData, setGenresData] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const genreforURL =useGenres(selectedGenres)
+  
+  useEffect(()=>{
+    const fetchMoviesApi = async()=>{
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+      );
+      setContent(data.results);
+      setNumOfPages(data.total_pages);
+    }
+    fetchMoviesApi()
+  },[page,genreforURL]);
+   
   return (
-    <div>Movies</div>
+    <div>
+       <span className='pageTitle'>Movies</span>
+       <Genres type="movies" 
+       selectedGenres={selectedGenres}
+       setSelectedGenres={setSelectedGenres}
+       genresData={genresData}
+       setGenresData={setGenresData}
+       setPage={setPage}
+       />
+       <div className='trending'>
+          {content && content.map((c)=>{
+            return <SingleContent key={c.id}
+            id={c.id}
+           poster={c.poster_path}
+           title={c.title || c.name}
+           date={c.release_date || c.first_air_date}
+           media="movie"
+           vote_average={c.vote_average}/>
+          })}
+       </div>
+       {numOfPages > 1 &&(
+         <CustomPagination setPage={setPage} numOfPages={numOfPages}/>
+       )}
+    </div>
   )
 }
